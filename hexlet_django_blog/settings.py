88e8305b2e86fde739eacbe893e3f396e7edfcb3
Sachkov Ilya <sos31337@gmail.com>
2023-01-10 04:47:05 +0300

    Update step by step

--- hexlet_django_blog/settings.py
+++ hexlet_django_blog/settings.py
@@ -56,7 +56,10 @@ ROOT_URLCONF = 'hexlet_django_blog.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [BASE_DIR / 'templates'],
+        'DIRS': [
+            BASE_DIR / 'templates',
+            BASE_DIR / 'article/templates',
+        ],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
